9edeea80f5ec1ce48303d4c083db565a268ee077
kyawhla-commit <bwarpay.bp8@gmail.com>
2025-08-19 08:50:59 +0630

    add Item component property

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,14 +1,16 @@
-function Item() {
-  return <li>Item One</li>;
+function Item(props) {
+  return <li>
+    {props.name} ({props.value})
+  </li>;
 }
 export default function App() {
   return (
     <>
       <h1>Hello React</h1>
       <ul>
-        <Item />
-        <Item />
-        <Item />
+        <Item name="Apple" value="123"/>
+        <Item  name="Egg" value="333"/>
+        <Item name="Bread" value="222"/>
       </ul>
     </>
   );
