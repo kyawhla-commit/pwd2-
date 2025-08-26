1e31f4bd17647d5235021cdd3409dc339273f29f
kyawhla-commit <bwarpay.bp8@gmail.com>
2025-08-26 22:13:47 +0630

    add useContext feature for dark and light mode

--- hello-context/src/App.jsx
+++ hello-context/src/App.jsx
@@ -1,28 +1,13 @@
-import { useState } from "react"
 
-function Mode({ mode, setMode }) {
-  return (
-    <button onClick={() => {
-      if (mode === "dark") setMode("light");
-      else setMode("dark");
+import Header from "./Header";
+import { useContext } from "react";
+import { ModeContext } from "./ModeProvider";
 
-    }}>Mode</button>
-  )
-}
-function Header({ mode, setMode }) {
-  return (
-    <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
-      <h1>Title</h1>
-      <Mode mode={mode} setMode={setMode} />
-    </div>
-  )
-}
 export default function App() {
-  const [mode, setMode] = useState("dark");
-
+  const { mode } = useContext(ModeContext);
   return (
     <div style={{ height: 2000, background: mode === "dark" ? "black" : "white", color: mode === "dark" ? "white" : "black" }}>
-      <Header mode={mode} setMode={setMode} />
+      <Header />
     </div>
   )
 }
---
+++ hello-context/src/Header.jsx
@@ -0,0 +1,11 @@
+import Mode from "./Mode";
+
+
+export default function Header() {
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
+      <h1>Title</h1>
+      <Mode />
+    </div>
+  )
+}
