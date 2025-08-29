6bcc442f3f75f4a7415a7b62b6b7ffb6ed6d9aa6
kyawhla-commit <bwarpay.bp8@gmail.com>
2025-08-29 10:08:03 +0630

    add clear feature form forntend to backend

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -50,7 +50,9 @@ export default function App() {
   }
 
   const clear = () => {
-    setItems(items.filter(item => !item.done))
+    fetch(api, {method: "DELETE"}).then(res => {
+      queryClient.invalidateQueries("tasks")
+    })
   }
   if (error) {
     return <div>Something went wrong...</div>
