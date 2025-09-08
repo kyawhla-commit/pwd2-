30b72294a110b13f2099814300924d3ee0d36e02
kyawhla-commit <bwarpay.bp8@gmail.com>
2025-09-08 10:35:34 +0630

    add comment add form and add post form

--- social/src/pages/Home.jsx
+++ social/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material";
 import Post from "../components/Post";
+import Form from "../components/Form";
 
 export default function Home() {
     return <Box>
+        <Form/>
         <Post/>
         <Post/>
         <Post/>
     </Box>
 
-    // 1:19
 }
--- social/src/pages/View.jsx
+++ social/src/pages/View.jsx
@@ -1,12 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, Button, OutlinedInput } from "@mui/material";
 import Post from "../components/Post";
 import Comment from "../components/Comment";
+import { useForm } from "react-hook-form";
 
 export default function View() {
-    return <Box>
-        <Post/>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+    const {
+        register,
+        handleSubmit,
+    } = useForm();
+
+   const onSubmit = data => {
+        console.log(data)
+    }
+    return <Box sx={{ pb: 8 }}>
+        <Post />
+        <Comment />
+        <Comment />
+        <Comment />
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <OutlinedInput
+                fullWidth
+                multiline
+                sx={{ my: 2 }}
+                placeholder="Your comment"
+                {...register("content", { required: true })}
+            />
+
+            <Button type="submit" variant="contained" fullWidth> Add Comment</Button>
+        </form>
     </Box>
 }
