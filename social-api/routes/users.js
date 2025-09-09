ca49824aa99db95006fd5f67327c91cf9db5c04f
kyawhla-commit <bwarpay.bp8@gmail.com>
2025-09-09 11:10:21 +0630

    fix verify user bug

--- social-api/routes/users.js
+++ social-api/routes/users.js
@@ -4,18 +4,17 @@ const router = express.Router();
 const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
 
-const {auth} = require("../middlewares/auth")
-
+const { auth } = require("../middlewares/auth");
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { da } = require("@faker-js/faker");
 
-router.get('/verify', auth, async (res, req) => {
-    return res.json({
-        msg: 'user verification success'
-    })
-})
+router.get("/verify", auth, async (req, res) => {
+  res.json({
+    msg: "user verification success",
+  });
+});
 
 router.post("/login", async (req, res) => {
   const username = req.body?.username;
@@ -43,36 +42,36 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.post('/register', async (req, res) => {
-    const name = req.body?.name;
-    const username = req.body?.username;
-    const bio = req.body?.bio;
-    const password = req.body?.password;
+router.post("/register", async (req, res) => {
+  const name = req.body?.name;
+  const username = req.body?.username;
+  const bio = req.body?.bio;
+  const password = req.body?.password;
 
-    if(!name || !username || !password ) {
-        return res.status(400).json({
-            msg: "name , username and password required..."
-        })
-    }
+  if (!name || !username || !password) {
+    return res.status(400).json({
+      msg: "name , username and password required...",
+    });
+  }
 
-    try {
-        const hash = await bcrypt.hash(password, 10)
+  try {
+    const hash = await bcrypt.hash(password, 10);
 
+    const user = await prisma.user.create({
+      data: {
+        name,
+        username,
+        bio,
+        password: hash,
+      },
+    });
 
-        const user = await prisma.user.create({
-            data: {
-                name, username, bio, password: hash, 
-            }
-        })
-
-        return res.json(user);
-
-    } catch (error) {
-        res.status(500).json({
-            msg: error.message
-        });
-        
-    }
-})
+    return res.json(user);
+  } catch (error) {
+    res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
 module.exports = { usersRouter: router };
